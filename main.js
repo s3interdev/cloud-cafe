127dbc2f2d1308ecfb393721161f11136ab7a4c0
Michael Owuor <michael.owuor@gmail.com>
2023-01-12 18:19:38 +0300

    feat: getting documents

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /* import the functions you need from the SDKs needed */
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 import './style.css';
 
 /* the web app's firebase configuration */
@@ -18,3 +19,29 @@ const app = initializeApp(firebaseConfig);
 
 /* initialize services */
 const db = getFirestore(app);
+
+/* html dom elements */
+const cafeList = document.querySelector('#cafe-list');
+
+/* create element and render cafes */
+function renderCafe(doc) {
+	let li = document.createElement('li');
+	let name = document.createElement('span');
+	let city = document.createElement('span');
+
+	li.setAttribute('data-id', doc.id);
+	name.textContent = doc.data().name;
+	city.textContent = doc.data().city;
+
+	li.appendChild(name);
+	li.appendChild(city);
+
+	cafeList.appendChild(li);
+}
+
+/* get all documents from the cafes collection */
+const snapshot = await getDocs(collection(db, 'cafes'));
+
+snapshot.forEach((doc) => {
+	renderCafe(doc);
+});
