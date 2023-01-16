2920fd9f1bc466202cd446163a51cae889947bea
Michael Owuor <michael.owuor@gmail.com>
2023-01-16 22:46:59 +0300

    feat: get realtime updates (#15)
    * feat: saving data
    
    * feat: deleting data
    
    * feat: making queries
    
    * feat: get realtime updates

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 /* import the functions you need from the SDKs needed */
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { collection, getDocs } from 'firebase/firestore';
+import {
+	addDoc,
+	collection,
+	deleteDoc,
+	doc,
+	getDocs,
+	getFirestore,
+	onSnapshot,
+	orderBy,
+	query,
+	where,
+} from 'firebase/firestore';
 import './style.css';
 
 /* the web app's firebase configuration */
@@ -22,28 +32,104 @@ const db = getFirestore(app);
 
 /* html dom elements */
 const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
 
 /* create element and render cafes */
 function renderCafe(doc) {
 	let li = document.createElement('li');
 	let name = document.createElement('span');
 	let city = document.createElement('span');
+	let cross = document.createElement('div');
 
 	li.setAttribute('data-id', doc.id);
 	name.textContent = doc.data().name;
 	city.textContent = doc.data().city;
+	cross.textContent = 'x';
 
 	li.appendChild(name);
 	li.appendChild(city);
+	li.appendChild(cross);
 
 	cafeList.appendChild(li);
+
+	/* click event listener for deleting documents */
+	cross.addEventListener('click', (e) => {
+		e.stopPropagation();
+
+		/* get document id */
+		let id = e.target.parentElement.getAttribute('data-id');
+
+		/* delete data */
+		deleteDocument(id);
+	});
 }
 
 /* get all documents from the cafes collection */
-(async function getDocuments() {
-	const snapshot = await getDocs(collection(db, 'cafes'));
+// (async function getDocuments() {
+// 	try {
+// 		const cafeRef = collection(db, 'cafes');
+// 		// const snapshot = await getDocs(cafeRef);
+// 		// const qry = await query(cafeRef, where('city', '==', 'Mombasa'));
+// 		const qry = await query(cafeRef, orderBy('name'));
+// 		const snapshot = await getDocs(qry);
 
-	snapshot.forEach((doc) => {
-		renderCafe(doc);
+// 		snapshot.forEach((doc) => {
+// 			renderCafe(doc);
+// 		});
+
+// 		console.log('Documents successfully retrieved...');
+// 	} catch (err) {
+// 		console.error('Error retrieving documents: ', err);
+// 	}
+// })();
+
+/* realtime collection data */
+(async function rtUpdates() {
+	const cafeRef = collection(db, 'cafes');
+	const qry = await query(cafeRef, orderBy('name'));
+
+	onSnapshot(qry, (snapshot) => {
+		snapshot.docChanges().forEach((change) => {
+			if (change.type === 'added') {
+				/* render item to the dom */
+				renderCafe(change.doc);
+			} else if (change.type === 'removed') {
+				let li = cafeList.querySelector('[data-id=' + change.doc.id + ']');
+
+				/* remove item from the dom */
+				cafeList.removeChild(li);
+			}
+		});
 	});
 })();
+
+/* saving data to the cafes collection */
+async function addDocument() {
+	try {
+		const cafeRef = await addDoc(collection(db, 'cafes'), {
+			name: form.name.value,
+			city: form.city.value,
+		});
+
+		console.log('Document written with ID: ', cafeRef.id);
+	} catch (err) {
+		console.error('Error adding document: ', err);
+	}
+}
+
+/* click event listener for adding documents */
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	/* save data */
+	addDocument();
+
+	/* clear form */
+	form.name.value = '';
+	form.city.value = '';
+});
+
+/* delete document from the cafes collection */
+async function deleteDocument(id) {
+	await deleteDoc(doc(db, 'cafes', id));
+}
